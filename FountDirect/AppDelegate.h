fc843fecd19d210e836fddaae8bec4e8f5dd0fd9
Zach <xuzhangde@hotmail.com>
2016-06-27 10:46:37 -0400

    Initial Commit

---
+++ FountDirect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FountDirect
+//
+//  Created by Zhang Xu on 6/27/16.
+//  Copyright © 2016 Syw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FountDirect/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FountDirect
+//
+//  Created by Zhang Xu on 6/27/16.
+//  Copyright © 2016 Syw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
